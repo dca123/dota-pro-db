4eab73d2d7258d8b876232073bbe7fef8b3c04b4
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2025-06-21 15:03:13 +0530

    added remote db

--- database/db_check.go
+++ database/db_check.go
@@ -2,12 +2,25 @@ package database
 
 import (
 	"database/sql"
+	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/rubenv/sql-migrate"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
 func GetDb() *sql.DB {
+	useTurso := flag.Bool("turso", false, "Uses remote turso db")
+	flag.Parse()
+	if *useTurso == true {
+		return getTursoDb()
+	}
+	return getSqliteDb()
+}
+
+func getSqliteDb() *sql.DB {
 	migrations := &migrate.FileMigrationSource{Dir: "../../migrations"}
 	db, err := sql.Open("sqlite3", "../../dota-pro-games.db?foreign_keys=true")
 	if err != nil {
@@ -19,4 +32,30 @@ func GetDb() *sql.DB {
 	}
 	log.Printf("Applied %d migrations!\n", n)
 	return db
+}
+
+func getTursoDb() *sql.DB {
+	var databaseUrl = os.Getenv("TURSO_DATABASE_URL")
+	if databaseUrl == "" {
+		log.Fatalln("TURSO_DATABASE_URL is not set")
+	}
+	var authToken = os.Getenv("TURSO_AUTH_TOKEN")
+	if authToken == "" {
+		log.Fatalln("TURSO_AUTH_TOKEN is not set")
+	}
+
+	url := fmt.Sprintf("%s?authToken=%s", databaseUrl, authToken)
+	db, err := sql.Open("libsql", url)
+
+	migrations := &migrate.FileMigrationSource{Dir: "../../migrations"}
+	n, err := migrate.Exec(db, "sqlite3", migrations, migrate.Up)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Applied %d migrations!\n", n)
+	if err != nil {
+		log.Fatalf("Failed to open turso db %s: %s \n", url, err)
+	}
+	return db
+
 }
--- go.mod
+++ go.mod
@@ -14,10 +14,14 @@ require (
 	github.com/agnivade/levenshtein v1.1.1 // indirect
 	github.com/alexflint/go-arg v1.4.2 // indirect
 	github.com/alexflint/go-scalar v1.0.0 // indirect
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
 	github.com/bmatcuk/doublestar/v4 v4.6.1 // indirect
+	github.com/coder/websocket v1.8.12 // indirect
 	github.com/go-gorp/gorp/v3 v3.1.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/tursodatabase/libsql-client-go v0.0.0-20240902231107-85af5b9d094d // indirect
 	github.com/vektah/gqlparser/v2 v2.5.19 // indirect
+	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
 	golang.org/x/mod v0.20.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/tools v0.24.0 // indirect
